3d257f299f0394fef041c352304ba3f3cb0326f1
Muhammad Mujtaba <mujtabapakistan262@gmail.com>
2026-07-31 11:33:20 +0500

    Initial project setup: Flask config and dependencies

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///codementor.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+    GEMINI_MODEL = os.getenv("GEMINI_MODEL")
